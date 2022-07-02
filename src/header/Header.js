ea2dd34ffb4f1d675651c7a5fefd7ebc4e07af92
Subarna Shrestha <shresthaanthrax@gmail.com>
2022-07-02 09:37:07 +0545

    modified some design changes for header

--- src/header/Header.js
+++ src/header/Header.js
@@ -13,22 +13,31 @@ function Header() {
 
       <div className="header_nav">
         <div className="header_option">
-          <span className="header_optionOne">Hello Subarna</span>
-          <span className="header_optionTwo">Sign In</span>
+          {/* <span className="header_optionOne">MICE</span> */}
+          <span className="header_optionTwo">MICE</span>
         </div>
         <div className="header_option">
-          <span className="header_optionOne">MICE</span>
-          <span className="header_optionTwo">& MOUSEPAD</span>
+          {/* <span className="header_optionOne">MICE</span> */}
+          <span className="header_optionTwo">HEADPHONES</span>
         </div>
         <div className="header_option">
-          <span className="header_optionOne">HEADPHONE</span>
-          <span className="header_optionTwo"> & KEYBOARDS</span>
+          {/* <span className="header_optionOne">MICE</span> */}
+          <span className="header_optionTwo">MOUSEPAD</span>
+        </div>
+        <div className="header_option">
+          {/* <span className="header_optionOne">HEADPHONE</span> */}
+          <span className="header_optionTwo">KEYBOARDS</span>
         </div>
 
         <div className="header_search">
           <input className="header_searchInput" type="text"></input>
           {/* search logo after this*/}
           <SearchIcon className="header_searchIcon" />
+        </div>
+
+        <div className="header_option">
+          <span className="header_optionOne">Hello Subarna</span>
+          <span className="header_optionTwo">Sign In</span>
         </div>
 
         <div className="header_optionBasket">
